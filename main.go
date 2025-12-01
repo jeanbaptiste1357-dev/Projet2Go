2934d351521dc104e1b3b3b432a55c91c84e1d91
njeanbap <jeanbaptiste.nguyen@ynov.com>
2025-12-01 10:18:40 +0100

    api

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "html/template"
     "io"
     "log"
     "net/http"
 )
 
-// Struct qui représente l'API principale
 type API struct {
     Artists   string `json:"artists"`
     Locations string `json:"locations"`
@@ -17,58 +17,57 @@ type API struct {
 }
 
 type Artist struct {
-    ID         int      `json:"id"`
-    Name       string   `json:"name"`
-    Members    []string `json:"members"`
-    Image      string   `json:"image"`
-    FirstAlbum string   `json:"firstAlbum"`
-    CreationDate int    `json:"creationDate"`
+    ID           int      `json:"id"`
+    Name         string   `json:"name"`
+    Members      []string `json:"members"`
+    Image        string   `json:"image"`
+    FirstAlbum   string   `json:"firstAlbum"`
+    CreationDate int      `json:"creationDate"`
 }
 
-
 func main() {
-    // 1) On récupère l'API principale
+    // Quand l'utilisateur va sur http://localhost:8080/artists
+    http.HandleFunc("/artists", artistsHandler)
+
+    fmt.Println("Serveur lancé sur : http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func artistsHandler(w http.ResponseWriter, r *http.Request) {
+
+    // 1) On récupère les URLs de l'API
     resp, err := http.Get("https://groupietrackers.herokuapp.com/api")
     if err != nil {
-        log.Fatal(err)
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
     }
     defer resp.Body.Close()
 
-    // 2) On lit les données
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
+    body, _ := io.ReadAll(resp.Body)
 
-    // 3) On transforme le JSON en struct Go
     var api API
-    err = json.Unmarshal(body, &api)
+    json.Unmarshal(body, &api)
+
+    // 2) On récupère les artistes
+    respArtists, err := http.Get(api.Artists)
     if err != nil {
-        log.Fatal(err)
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer respArtists.Body.Close()
+
+    bodyArtists, _ := io.ReadAll(respArtists.Body)
+
+    var artists []Artist
+    json.Unmarshal(bodyArtists, &artists)
+
+    // 3) On charge le template
+    tmpl, err := template.ParseFiles("templates/artists.html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
     }
 
-    // 4) On vérifie que ça marche
-    fmt.Println("URL des artistes :", api.Artists)
-
-	// 5) On récupère les artistes
-respArtists, err := http.Get(api.Artists)
-if err != nil {
-    log.Fatal(err)
-}
-defer respArtists.Body.Close()
-
-bodyArtists, err := io.ReadAll(respArtists.Body)
-if err != nil {
-    log.Fatal(err)
-}
-
-var artists []Artist
-err = json.Unmarshal(bodyArtists, &artists)
-if err != nil {
-    log.Fatal(err)
-}
-
-// 6) Afficher le premier artiste pour vérifier
-fmt.Println("Premier artiste :", artists[0].Name)
-
+    // 4) On envoie les artistes dans le HTML
+    tmpl.Execute(w, artists)
 }
